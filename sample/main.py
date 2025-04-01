454aae594d0832257c56f8897723cf77eb270a69
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2025-04-01 10:06:17 +0530

    Create main.py

---
+++ sample/main.py
@@ -0,0 +1,15 @@
+def   BadlyFormattedFunction  (x,y):return   x+y
+
+class myclass :   def __init__(self):self.value=0
+def  method1 (self): self.value +=1
+def method2(self)  :return self.value
+
+def another_function   (   a,b  ,c  ) :
+ print ( "Sum is:", a+ b +c ) 
+
+ if a> b:
+  print("A is greater")  
+ else:
+        print("B is greater or equal")   
+        
+def    very_bad_spacing():a=10  ;b=20 ;print(a+b)
